d3d01ca3920f5cbc4b33f8fd216492b50a501540
Yasser Hajlaoui <yasser159@gmail.com>
2022-04-23 23:42:46 -0700

    initial commit

--- QR_Maker_Code/ViewController.swift
+++ QR_Maker_Code/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txt_Link: UITextField!
+    @IBOutlet weak var img_QR_ImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func btn_Generate(_ sender: Any) {
+        
+        if let linkText = txt_Link.text {
+            let myImage = generateQRCode(from: linkText)
+            img_QR_ImageView.image = myImage
+        }
+        
+    }
+    
+    
+    func generateQRCode(from string: String) -> UIImage? {
+        let data = string.data(using: String.Encoding.ascii)
+
+        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+            filter.setValue(data, forKey: "inputMessage")
+            let transform = CGAffineTransform(scaleX: 6, y: 6)
+
+            if let output = filter.outputImage?.transformed(by: transform) {
+                return UIImage(ciImage: output)
+            }
+        }
+
+        return nil
+    }
+
+    
+    
+    
 }
 
