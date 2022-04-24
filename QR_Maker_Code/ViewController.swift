d578e4a0d3e68bfa6b1c25533dc847f9a22954f1
Yasser Hajlaoui <yasser159@gmail.com>
2022-04-23 22:40:21 -0700

    Initial Commit

---
+++ QR_Maker_Code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QR_Maker_Code
+//
+//  Created by Yasser Hajlaoui on 4/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
